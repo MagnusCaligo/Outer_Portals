0587c0d7b470d53f447cd87665e3b9f527b9c7c5
Magnus <jaloeffler@gmail.com>
2024-08-03 22:39:18 -0700

    got mirror portal working

---
+++ First Test Mod.cs
@@ -0,0 +1,97 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+namespace First_Test_Mod;
+[HarmonyPatch]
+public class First_Test_Mod : ModBehaviour
+{
+	public static First_Test_Mod Instance;
+public INewHorizons NewHorizons;
+
+    private AssetBundle bundle;
+
+public void Awake()
+{
+    Instance = this;
+    Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        // You won't be able to access OWML's mod helper in Awake.
+        // So you probably don't want to do anything here.
+        // Use Start() instead.
+    }
+
+public void Start()
+{
+    // Starting here, you'll have access to OWML's mod helper.
+    ModHelper.Console.WriteLine($"My mod {nameof(First_Test_Mod)} is loaded!", MessageType.Success);
+
+    // Get the New Horizons API and load configs
+    NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+    NewHorizons.LoadConfigs(this);
+
+    new Harmony("Mags.First Test Mod").PatchAll(Assembly.GetExecutingAssembly());
+
+    // Example of accessing game code.
+    OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+    //bundle = ModHelper.Assets.LoadBundle("assets/magsbundle");
+    LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+
+}
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(GhostData), nameof(GhostData.FixedUpdate_Data))]
+    public static bool GhostData_FixedUpdate_Data_Prefix(GhostData __instance)
+    {
+        return false;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Campfire), nameof(Campfire.OnPressInteract))]
+    public static void Campfire_OnPressInteract_Prefix(Campfire __instance)
+    {
+        First_Test_Mod.Instance.ModHelper.Console.WriteLine($"Sector is {__instance.GetSector()}");
+        First_Test_Mod.Instance.ModHelper.Console.WriteLine($"Sector is {__instance.GetType()}");
+
+    }
+
+
+public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+    {
+        if (newScene != OWScene.SolarSystem) return;
+        ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+
+        GlobalMessenger.AddListener("WakeUp", () => { StartCoroutine("helper_function"); });
+
+        /*
+        var prefab = bundle.LoadAsset("Assets/Custom Prefabs/Mirror.prefab");
+        GameObject.Instantiate(prefab);
+        prefab = bundle.LoadAsset("Assets/Custom Prefabs/BasicTestObject.prefab");
+        GameObject.Instantiate(prefab);
+        */
+
+    }
+
+    public IEnumerator helper_function()
+    {
+        First_Test_Mod.Instance.ModHelper.Console.WriteLine("Calling Helper function");
+        yield return new WaitForSeconds(3);
+        movement_unlocked();
+        
+    }
+
+   // [HarmonyPostfix]
+    //[HarmonyPatch(typeof(PlayerCharacterController), nameof(PlayerCharacterController.OnPlayerResurrection))]
+    public static void movement_unlocked()
+    {
+
+        First_Test_Mod.Instance.ModHelper.Console.WriteLine("Should be putting on suit");
+        DreamLanternItem newLantern = new DreamLanternItem();
+        Locator.GetPlayerSuit().SuitUp(false, false, true);
+        Locator.GetPlayerController().SetDreamLantern(newLantern);
+   
+    }
+}
+
---
+++ src/VolmetricLine.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UIElements;
+using static ShapeUtil;
+using static UnityEngine.UI.Image;
+
+[ExecuteInEditMode]
+public class VolumetricLine : MonoBehaviour
+{
+
+    public float width = 5f;
+    public Vector3[] vertices;
+    public GameObject childrenComponents;
+    public GameObject childrenObjects;
+    public bool initialized = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void makeChildrenComponents()
+    {
+        Debug.Log("Making Object");
+        childrenComponents = new GameObject("ChildrenComponents");
+        childrenComponents.transform.parent = transform;
+        var directionalForce = childrenComponents.AddComponent<DirectionalForceVolume>();
+        directionalForce._fieldDirection = new Vector3(0, 0, -1f);
+        directionalForce.SetFieldMagnitude(10);
+        childrenComponents.AddComponent<OxygenVolume>();
+        initialized = true;
+        Debug.Log("Finished making object");
+    }
+
+    void makeChildrenObjects()
+    {
+        childrenObjects = new GameObject("ChildrenObjects");
+        childrenObjects.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (initialized)
+            return;
+        initialized = true;
+        // Destroy all children
+        if (childrenObjects != null)
+            DestroyImmediate(childrenObjects);
+
+
+        // Check if children components exists
+        if (childrenComponents == null)
+            makeChildrenComponents();
+
+        makeChildrenObjects();
+
+        if (vertices.Length < 2)
+            return;
+        for (int i = 0; i < vertices.Length - 1; i++)
+        {
+            Vector3 direction = vertices[i + 1] - vertices[i];
+            Vector3 between_points = Vector3.Normalize(vertices[i + 1] - vertices[i]);
+            float distance_between = Vector3.Distance(vertices[i], vertices[i + 1]);
+            GameObject cylinder = createVolume(width, Vector3.Distance(vertices[i], vertices[i + 1]));
+            cylinder.transform.parent = childrenObjects.transform;
+            cylinder.transform.localRotation = Quaternion.LookRotation(direction.normalized);
+            cylinder.transform.localRotation *= Quaternion.Euler(90, 0, 0);
+            cylinder.transform.localPosition = vertices[i] + (between_points * (distance_between/2.0f));
+            cylinder.name = "cylinder_" + i;
+        }
+    }
+
+    public void AddChildrenComponents(ref GameObject obj)
+    {
+        var directionalForce = obj.AddComponent<DirectionalForceVolume>();
+        directionalForce._fieldDirection = new Vector3(0, 0, 1f);
+        directionalForce.SetFieldMagnitude(10);
+        obj.AddComponent<OxygenVolume>();
+    }
+
+    GameObject createVolume(float width, float length)
+    {
+        GameObject volume = new GameObject();
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        GameObject topSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        GameObject bottomSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        DestroyImmediate(cylinder.GetComponent<MeshRenderer>());
+        DestroyImmediate(topSphere.GetComponent<MeshRenderer>());
+        DestroyImmediate(bottomSphere.GetComponent<MeshRenderer>());
+
+        // Add Children Components
+        AddChildrenComponents(ref cylinder);
+        AddChildrenComponents(ref topSphere);
+        AddChildrenComponents(ref bottomSphere);
+
+        // Add to parent
+        cylinder.transform.parent = volume.transform;
+        topSphere.transform.parent = volume.transform;
+        bottomSphere.transform.parent = volume.transform;
+
+        cylinder.transform.localScale = new Vector3(width, length/2.0f, width);
+        topSphere.transform.localPosition = new Vector3(0, length/2.0f, 0);
+        bottomSphere.transform.localPosition = new Vector3(0, -length/2.0f, 0);
+
+        topSphere.transform.localScale = new Vector3(width, width, width);
+        bottomSphere.transform.localScale = new Vector3(width, width, width);
+
+        return volume;
+    }
+
+}
